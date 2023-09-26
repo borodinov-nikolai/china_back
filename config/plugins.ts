05bc6f9542f693310f828739b6e18755f477f52b
4abaev <progchab@gmail.com>
2023-09-26 15:49:13 +0300

    feat: confirm email, nodemailer config

--- config/plugins.ts
+++ config/plugins.ts
@@ -14,4 +14,27 @@ export default {
       enabled: true,
       resolve: './src/plugins/control-panel'
     },
+    email: {
+      config: {
+        provider: 'nodemailer',
+        providerOptions: {
+          host: process.env.SMTP_HOST,
+          port: process.env.SMTP_PORT,
+          service: process.env.SMTP_SERVICE,
+          auth: {
+            user: process.env.SMTP_USERNAME,
+            pass: process.env.SMTP_PASSWORD,
+          },
+          pool: true,
+          logger: true,
+          debug: true,
+          maxConnections: 10000
+        },
+        
+        settings: {
+          defaultFrom: process.env.DEFAULT_EMAIL,
+          defaultReplyTo: process.env.DEFAULT_EMAIL,
+        },
+      },
+    },
   }
