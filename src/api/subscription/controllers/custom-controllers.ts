9d5f2d348af25968c53bb2206d4b71d09083989a
4abaev <progchab@gmail.com>
2023-10-11 17:21:16 +0300

    fix: youcassa

--- src/api/subscription/controllers/custom-controllers.ts
+++ src/api/subscription/controllers/custom-controllers.ts
@@ -5,7 +5,7 @@ export default factories.createCoreController(
   ({strapi}) => ({
     async createSubscription(ctx) {
       const currentUser = ctx.state.user.id;
-      const {tariff_id} = ctx.request.body;
+      const tariff_id = Number(ctx.request.body.tariff_id);
       const date = new Date();
       const user = await strapi.entityService.findOne(
         "plugin::users-permissions.user",
@@ -15,32 +15,27 @@ export default factories.createCoreController(
           populate: {subscriptions: true},
         }
       );
-      // какого дня оформлена подписка
+      const tariff = await strapi.entityService.findOne(
+        "api::tariff.tariff",
+        tariff_id,
+        {
+          fields: ["month", "price"],
+          populate: {subscriptions: true},
+        }
+      );
       let startDay =
         user.subscriptions.length > 0
           ? new Date(user.subscriptions.at(-1).dueToDay)
           : new Date(date.toISOString().split("T")[0]);
-      // от какого числа продливать подписку
       let lastDay =
         user.subscriptions.length > 0
           ? new Date(user.subscriptions.at(-1).dueToDay)
           : new Date(date.toISOString().split("T")[0]);
 
-      function addMonth(tariffId: number) {
-        switch (tariffId) {
-          case 1:
-            return new Date(lastDay.setMonth(lastDay.getMonth() + 1))
-              .toISOString()
-              .split("T")[0];
-          case 2:
-            return new Date(lastDay.setMonth(lastDay.getMonth() + 3))
-              .toISOString()
-              .split("T")[0];
-          case 3:
-            return new Date(lastDay.setMonth(lastDay.getMonth() + 6))
-              .toISOString()
-              .split("T")[0];
-        }
+      function addMonth(month: number) {
+        return new Date(lastDay.setMonth(lastDay.getMonth() + month))
+          .toISOString()
+          .split("T")[0];
       }
 
       return await strapi.entityService.create(
@@ -48,8 +43,8 @@ export default factories.createCoreController(
         {
           data: {
             startDay,
-            dueToDay: addMonth(tariff_id),
-            tariff_id,
+            dueToDay: addMonth(tariff.month),
+            tariff_id: Number(tariff_id),
             user_id: currentUser,
           },
         }
@@ -71,7 +66,28 @@ export default factories.createCoreController(
         return order;
     },
     async payOrder(ctx) {
-      console.log(ctx.request.body)
+      const payment = ctx.request.body.object
+      if (payment.status == 'succeeded') {
+        const order = await strapi.query('api::order.order').update({
+          where: { payment_id: payment.id },
+          data: {
+            isPayed: true,
+          },
+        });
+        ctx = {
+          request: {
+            body: {
+              tariff_id: order.tariff_id
+            }
+          },
+          state: {
+            user: {
+              id: order.user_id
+            }
+          }
+        }
+        return await this.createSubscription(ctx);
+      }
     }
   })
 );
