21aab8d63895d1f8843dfb0edcd1f4ccab8a539b
Pavarha Maksim <dilexet@gmail.com>
2023-12-11 18:25:29 +0300

    Changed postgres pool

--- config/database.ts
+++ config/database.ts
@@ -68,7 +68,16 @@ export default ({ env }) => {
         },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      pool: {
+        min: 0,
+        max: 4,
+        acquireTimeoutMillis: 300000,
+        createTimeoutMillis: 300000,
+        destroyTimeoutMillis: 300000,
+        idleTimeoutMillis: 30000,
+        reapIntervalMillis: 1000,
+        createRetryIntervalMillis: 2000,
+      },
     },
     sqlite: {
       connection: {
