95a1d4a95d7065fd976d42d8054d6425709518ff
Pavarha Maksim <dilexet@gmail.com>
2023-12-19 10:47:21 +0300

    added logs

--- src/api/srt/controllers/srt.ts
+++ src/api/srt/controllers/srt.ts
@@ -3,21 +3,30 @@ import Parser, { getIam, segmentate, translateChineseSubtitles, rebuildAndRestar
 
 export default {
   exampleAction: async (ctx)  => {
+    console.log("Started")
     const srtFilePath =  "public" + ctx.request.body.url;
     const uploadDir = 'public/uploads';
     try {
+      console.log("Before readdirSync")
       fs.readdirSync(uploadDir).forEach(file => {
         const filePath = uploadDir + "/" + file;
         if (filePath !== srtFilePath && filePath !== 'public/uploads/.gitkeep') {
           fs.unlinkSync(filePath); // Удаляем файл, если это не тот, что в srtFilePath
         }
       });
+      console.log("After readdirSync")
       const srtData = fs.readFileSync(srtFilePath, "utf-8");
+      console.log("After srtData")
       const parser = new Parser();
+      console.log("After parser")
       const json = parser.fromSrt(srtData);
+      console.log("After json")
       const segmentated = await segmentate(json);
+      console.log("After segmentated")
       const { iamToken } = await getIam()
+      console.log("After iamToken")
       await translateChineseSubtitles(json, iamToken)
+      console.log("After translateChineseSubtitles")
 
       fs.writeFile(
         "public/uploads/zh_subtitles.json",
